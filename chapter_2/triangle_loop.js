f14d18b64244fdef4982aa47562bb2246a2c16c9
austiezr <austinziech@gmail.com>
2020-09-28 17:08:44 -0600

    completed chessboard, reorganized directories, completed minimum

---
+++ chapter_2/triangle_loop.js
@@ -0,0 +1,9 @@
+const writeTriangle = function() {
+    let out = '#';
+    while (out.length <= 7) {
+        console.log(out);
+        out += '#';
+    }
+};
+
+console.log(writeTriangle());
---
+++ chapter_3/minimum.js
@@ -0,0 +1,19 @@
+const minimum = function(a, b) {
+    switch (true) {
+        case a < b:
+            return a;
+        case b < a:
+            return b;
+        case a === b:
+            console.log('Numbers are equal');
+            break;        
+        default:
+            console.error('Input Error');
+            break;
+    }
+};
+
+console.log(minimum(-10, 1));
+console.log(minimum(1, 10));
+console.log(minimum(10, 10));
+console.log(minimum('f', 4));
