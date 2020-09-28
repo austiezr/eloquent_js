042d02489550036c5f6a36f1e5c05f84d076a9e8
austiezr <austinziech@gmail.com>
2020-09-28 16:56:06 -0600

    finished chessboard with variable grid size

---
+++ chessboard.js
@@ -0,0 +1,20 @@
+const makeBoard = function(size = 8) {
+    if (size % 2 != 0) {
+        var out = ' #'.repeat(size/2) + ' '
+        var out_alt = '# '.repeat(size/2) + '#'
+    }
+    else {
+        var out = ' #'.repeat(size/2)
+        var out_alt = '# '.repeat(size/2)
+    }
+    for (let index = 0; index < size; index++) {
+        if (index % 2 === 0) {
+            console.log(out)
+        }
+        else {
+            console.log(out_alt)
+        }
+    }
+}
+
+console.log(makeBoard(9))
